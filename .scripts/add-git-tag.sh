32abbf86c0e6d9032bd616d7bcfedccf39117b27
Travis CI <builds@travis-ci.com>
2015-09-02 16:28:03 +0300

    adding bintray

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -5,7 +5,5 @@ export TAG_NAME=$VERSION
 echo "Tagging with $TAG_NAME"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-echo 1
 git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $TAG_NAME
-echo 2
 git push https://mmyslyvtsev:$GITHUBKEY@github.com/mmyslyvtsev/girls-monitor.git --tags
---
+++ .scripts/prepare-bintray.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+export VERSION=$(ruby -r rexml/document -e 'puts REXML::Document.new(File.new(ARGV.shift)).elements["/project/version"].text' pom.xml)
+export DATE=$(date +"%Y-%m-%d")
+export DESCRIPTION=$(git log --oneline -n1)
+export DESCRIPTION=$(echo $DESCRIPTION | sed -e 's/[^ ]* //')
+export DESCRIPTION=${DESCRIPTION:0:50}
+export DESCRIPTION=$(echo $DESCRIPTION | python -c 'import json,sys; print json.dumps(sys.stdin.read()[:-1])[1:-1]')
+
+echo "Setting version to $VERSION"
+echo "Setting date to $DATE"
+echo "Setting description to \"$DESCRIPTION\""
+
+sed -i.bak -e "s/__VERSION__/$VERSION/g" -e "s/__DATE__/$DATE/g" -e "s/__DESCRIPTION__/$DESCRIPTION/g" .scripts/bintray.json
