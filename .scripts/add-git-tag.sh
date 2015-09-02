2f18df0e928767afcdfce89a1a154ec302f9a998
Travis CI <builds@travis-ci.com>
2015-09-02 15:25:51 +0300

    fixing scripts

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
-export TAG_NAME=$NEW_VERSION
+export VERSION=$(ruby -r rexml/document -e 'puts REXML::Document.new(File.new(ARGV.shift)).elements["/project/version"].text' pom.xml)
+export TAG_NAME=$VERSION
 echo "Tagging with $TAG_NAME"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 echo 1
 git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $TAG_NAME
 echo 2
-git push https://github.com/mmyslyvtsev/girls-monitor.git --tags
+git push https://mmyslyvtsev:$GITHUBKEY@github.com/mmyslyvtsev/girls-monitor.git --tags
