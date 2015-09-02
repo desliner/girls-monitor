b74d7f940a2d334424ab0403a4faef9e57b6df06
Max Myslyvtsev <mmyslyvtsev@griddynamics.com>
2015-09-02 15:05:31 +0300

    fixing scripts

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -2,5 +2,5 @@
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git tag $NEW_VERSION -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git push -q https://$TAGPERM@github.com/mmyslyvtsev/girls-monitor --tags
+git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $NEW_VERSION
+git push origin $NEW_VERSION
