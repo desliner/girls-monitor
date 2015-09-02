b119c095b9616ab2838b86890d9366a240c7544b
Travis CI <builds@travis-ci.com>
2015-09-02 15:18:07 +0300

    fixing scripts

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
 export TAG_NAME=$NEW_VERSION
+echo "Tagging with $TAG_NAME"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
+echo 1
 git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $TAG_NAME
-git push origin --tags
+echo 2
+git push https://github.com/mmyslyvtsev/girls-monitor.git --tags
