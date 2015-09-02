220b9fc13f886e0b4d1610eef452a108f3279a58
mmyslyvtsev <mmyslyvtsev@griddynamics.com>
2015-09-02 17:44:42 +0300

    fixing scripts

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -6,5 +6,6 @@ echo "Tagging with $TAG_NAME"
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $TAG_NAME
-echo "https://mmyslyvtsev:$GITHUBKEY@github.com" >> ~/.git-credentials
+echo "machine github.com login mmyslyvtsev password $GITHUBKEY" > ~/.netrc
+chmod 600 ~/.netrc
 git push https://github.com/mmyslyvtsev/girls-monitor.git --tags
