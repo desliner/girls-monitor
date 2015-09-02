e28b6ed98c75cd609aa0c8c7426279465cb6301d
Max Myslyvtsev <mmyslyvtsev@griddynamics.com>
2015-09-02 15:11:23 +0300

    fixing scripts

--- .scripts/add-git-tag.sh
+++ .scripts/add-git-tag.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
+export TAG_NAME=$NEW_VERSION
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $NEW_VERSION
-git push origin $NEW_VERSION
+git tag -a -m "Generated tag from TravisCI for build $TRAVIS_BUILD_NUMBER" $TAG_NAME
+git push origin --tags
--- .scripts/update-version.sh
+++ .scripts/update-version.sh
@@ -2,4 +2,5 @@
 
 export CURRENT_VERSION=$(ruby -r rexml/document -e 'puts REXML::Document.new(File.new(ARGV.shift)).elements["/project/version"].text' pom.xml)
 export NEW_VERSION=$(echo $CURRENT_VERSION | sed -e "s/-SNAPSHOT/.$TRAVIS_BUILD_NUMBER/g")
+echo "Changing version from $CURRENT_VERSION to $NEW_VERSION"
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
